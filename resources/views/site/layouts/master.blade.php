26d2ca9be6c679385071347ec30432dc3b1a2b1c
thang197 <thangnguyennvt1410@gmail.com>
2025-11-13 17:16:06 +0700

    F

--- resources/views/site/layouts/master.blade.php
+++ resources/views/site/layouts/master.blade.php
@@ -4,9 +4,41 @@
 <head>
     @include('site.partials.head')
     @yield('css')
+
+    <!-- Meta Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '2282309942215854');
+        fbq('track', 'PageView');
+    </script>
+
+    <!-- End Meta Pixel Code -->
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-8C0M33482V"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-8C0M33482V');
+    </script>
+
 </head>
 
 <body ng-app="App">
+
+<noscript><img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=2282309942215854&ev=PageView&noscript=1"
+    /></noscript>
+
 {{--    <div class="preloader">--}}
 {{--        <div class="preloader-inner">--}}
 {{--            <div class="loader"><span></span> <span></span> <span></span> <span></span> <span></span> <span></span></div>--}}
