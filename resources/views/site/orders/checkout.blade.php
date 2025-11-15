3c91cfe5959ca037489cd722291c4d4f60fb4220
thang197 <thangnguyennvt1410@gmail.com>
2025-11-15 17:00:28 +0700

    F

--- resources/views/site/orders/checkout.blade.php
+++ resources/views/site/orders/checkout.blade.php
@@ -2545,9 +2545,28 @@
                 });
         });
     </script>
+
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '2282309942215854');
+        fbq('track', 'PageView');
+
+    </script>
+
 </head>
 
 <body ng-app="App" ng-controller="CheckoutController" ng-cloak>
+<noscript><img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=2282309942215854&ev=PageView&noscript=1"
+    /></noscript>
+
     <div class="banner">
         <div class="wrap">
             <a href="{{route('front.home-page')}}" class="logo">
@@ -3278,6 +3297,12 @@
 
             $scope.submitOrder = function() {
                 $scope.loading = true;
+
+                if (typeof fbq !== 'undefined') {
+                    fbq('track', 'AddPaymentInfo');
+                }
+
+
                 let data = $scope.form;
                 data.discount_code = $scope.discount.code;
                 data.discount_value = $scope.discount.value;
